486e72524b07fa8748b3b131dbb83a44cd1bb7b8
Alessandro Vozza <alessandro@ams0.org>
2015-07-11 09:19:38 +0200

    now uses openstack client

--- evacuate_failed_host.sh
+++ evacuate_failed_host.sh
@@ -5,10 +5,10 @@
 # Credentials must be sourced before using the script
 HOSTDOWN=`nova service-list | grep nova-compute | grep down | awk '{print $6}'`
 echo "Host $HOSTDOWN is down!"
-SERVERDOWN=`nova list --all-tenants --field name,status,OS-EXT-SRV-ATTR:host | grep $HOSTDOWN |  awk '{print $4}'`
+SERVERDOWN=`openstack server list --os-cloud mycloud --long --all-projects | grep $HOSTDOWN |  awk '{print $4}'`
 echo "Found these instances on failed host:"
 echo $SERVERDOWN
 for i in $SERVERDOWN; do
   echo "Evacuating server $i"
-  nova evacuate --on-shared-storage $i
+  openstack server migrate --os-cloud mycloud --shared-migration $i
 done
