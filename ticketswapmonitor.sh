34c6137b735b441e67ee60d5983033885d782614
tolgita <tolgacatalkaya@gmail.com>
2015-07-13 13:38:09 +0200

    Update ticketswapmonitor.sh

--- ticketswapmonitor.sh
+++ ticketswapmonitor.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # usage: screen watch ticketswapmonitor.sh
 
-result=`curl -s https://www.ticketswap.nl/60885/welcome-to-the-future-festival-2015/1338/festival | grep -A2 "Aangeboden" | grep counter-value | awk 'NR > 1 {print $1}' RS='>' FS='<' | head -n 1`
+result=`curl -s https://www.ticketswap.nl/75341/voltt-loves-summer-festival | grep -A2 "Aangeboden" | grep counter-value | awk 'NR > 1 {print $1}' RS='>' FS='<' | head -n 1`
 
 if [[ $result -eq 0 ]]
 then
   echo $"nothing"
 else
-  echo "New tickets for WTTF2015" | mail -s "New tickets for WTTF2015" alessandro@ams0.org
+  echo "New tickets for WTTF2015" | mail -s "New tickets for WTTF2015" tolgacatalkaya@gmail.com
   sleep 10
 fi
